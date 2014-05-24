a46880d6def77aab9cab1457bbcff01d6e0b8b86
David Stockinger <dave.stockinger@gmail.com>
2014-05-24 17:48:15 +0200

    Added Graphic

---
+++ BahnEditor/BahnEditor.BahnLib/Graphic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BahnEditor.BahnLib
+{
+	public class Graphic
+	{
+		public Graphic Load(string path) {
+			if (File.Exists(path))
+			{
+				return Load(File.OpenRead(path));
+			}
+			else throw new FileNotFoundException("File not found", path);
+		}
+
+		private Graphic Load(FileStream path)
+		{
+			//TODO Implement Load(FileStream)
+
+			throw new NotImplementedException();
+		}
+
+		public bool Save(string path, bool overwrite)
+		{
+			if (File.Exists(path) && !overwrite)
+			{
+				return false;
+			}
+			else
+			{
+				return Save(File.OpenWrite(path));
+			}
+		}
+
+		private bool Save(FileStream path)
+		{
+			try
+			{
+				//TODO Implement Save(FileStream)
+
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
