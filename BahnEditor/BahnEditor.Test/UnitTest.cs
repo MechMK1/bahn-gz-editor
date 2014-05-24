37001580de22c09e0488cdc44d1cd74a5bd264c4
David Stockinger <dave.stockinger@gmail.com>
2014-05-24 16:46:26 +0200

    Added Unit Tests

---
+++ BahnEditor/BahnEditor.Test/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BahnEditor.Test
+{
+	[TestClass]
+	public class UnitTest
+	{
+		
+		[TestMethod]
+		public void TestMethod()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
