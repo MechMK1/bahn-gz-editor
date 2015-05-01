f8ebaf15d183aa8e3c16e681cb5a800c6527f210
MechMK1 <dave.stockinger@gmail.com>
2015-05-01 10:30:06 +0200

    Moved ArchiveElement to GraphicArchive

--- BahnEditor/BahnEditor.BahnLib/GraphicArchive.cs
+++ BahnEditor/BahnEditor.BahnLib/GraphicArchive.cs
@@ -350,5 +350,52 @@ namespace BahnEditor.BahnLib
 			}
 		}
 		#endregion Private Methods
+
+		#region Nested Classes
+		/// <summary>
+		/// Represents an element in a graphic archive
+		/// </summary>
+		private class ArchiveElement
+		{
+			#region Properties
+			/// <summary>
+			/// Index in the archive
+			/// </summary>
+			public int ElementNumber { get; set; }
+
+			/// <summary>
+			/// Animationphase
+			/// </summary>
+			public int AnimationPhase { get; set; }
+
+			/// <summary>
+			/// Alternative
+			/// </summary>
+			public int Alternative { get; set; }
+
+			/// <summary>
+			/// Graphic
+			/// </summary>
+			public Graphic Graphic { get; set; }
+
+			/// <summary>
+			/// Seekposition in the archive-file (prepared for later)
+			/// </summary>
+			public int SeekPosition { get; set; }
+
+			public long SeekPositionGraphicData { get; set; }
+			#endregion Properties
+
+			#region Constructor
+			internal ArchiveElement(int elementNumber, int animationPhase, int alternative, Graphic graphic)
+			{
+				this.ElementNumber = elementNumber;
+				this.AnimationPhase = animationPhase;
+				this.Alternative = alternative;
+				this.Graphic = graphic;
+			}
+			#endregion
+		}
+		#endregion Nested Classes
 	}
 }
