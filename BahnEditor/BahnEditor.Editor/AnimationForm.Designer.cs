e887401735d8439b9fa7f20079ca59326d1634f2
MisterMarty <martin.diesenreither@gmx.at>
2015-05-18 21:08:26 +0200

    Added adding, deleting and moving of animationsteps

--- BahnEditor/BahnEditor.Editor/AnimationForm.Designer.cs
+++ BahnEditor/BahnEditor.Editor/AnimationForm.Designer.cs
@@ -33,11 +33,18 @@
 			this.minTimeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.maxTimeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.soundColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.noAnimationLabel = new System.Windows.Forms.Label();
+			this.addAnimationStepButton = new System.Windows.Forms.Button();
+			this.deleteAnimationStepButton = new System.Windows.Forms.Button();
+			this.upAnimationStepButton = new System.Windows.Forms.Button();
+			this.downAnimationStepButton = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dataGridView
 			// 
+			this.dataGridView.AllowDrop = true;
+			this.dataGridView.AllowUserToAddRows = false;
 			this.dataGridView.AllowUserToResizeColumns = false;
 			this.dataGridView.AllowUserToResizeRows = false;
 			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -49,11 +56,19 @@
             this.minTimeColumn,
             this.maxTimeColumn,
             this.soundColumn});
-			this.dataGridView.Location = new System.Drawing.Point(13, 51);
+			this.dataGridView.Location = new System.Drawing.Point(12, 12);
 			this.dataGridView.MultiSelect = false;
 			this.dataGridView.Name = "dataGridView";
-			this.dataGridView.Size = new System.Drawing.Size(460, 307);
+			this.dataGridView.RowHeadersWidth = 20;
+			this.dataGridView.Size = new System.Drawing.Size(371, 308);
 			this.dataGridView.TabIndex = 0;
+			this.dataGridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView_CellValidating);
+			this.dataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+			this.dataGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView_UserDeletingRow);
+			this.dataGridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView_DragDrop);
+			this.dataGridView.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGridView_DragOver);
+			this.dataGridView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseDown);
+			this.dataGridView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseMove);
 			// 
 			// phaseColumn
 			// 
@@ -80,18 +95,81 @@
 			this.soundColumn.HeaderText = "Sound";
 			this.soundColumn.Name = "soundColumn";
 			// 
+			// noAnimationLabel
+			// 
+			this.noAnimationLabel.AutoSize = true;
+			this.noAnimationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.noAnimationLabel.Location = new System.Drawing.Point(13, 13);
+			this.noAnimationLabel.Name = "noAnimationLabel";
+			this.noAnimationLabel.Size = new System.Drawing.Size(308, 18);
+			this.noAnimationLabel.TabIndex = 1;
+			this.noAnimationLabel.Text = "No animation defined for the selected graphic.";
+			// 
+			// addAnimationStepButton
+			// 
+			this.addAnimationStepButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.addAnimationStepButton.Location = new System.Drawing.Point(389, 12);
+			this.addAnimationStepButton.Name = "addAnimationStepButton";
+			this.addAnimationStepButton.Size = new System.Drawing.Size(23, 23);
+			this.addAnimationStepButton.TabIndex = 3;
+			this.addAnimationStepButton.Text = "+";
+			this.addAnimationStepButton.UseVisualStyleBackColor = true;
+			this.addAnimationStepButton.Click += new System.EventHandler(this.addAnimationStepButton_Click);
+			// 
+			// deleteAnimationStepButton
+			// 
+			this.deleteAnimationStepButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.deleteAnimationStepButton.Location = new System.Drawing.Point(389, 41);
+			this.deleteAnimationStepButton.Name = "deleteAnimationStepButton";
+			this.deleteAnimationStepButton.Size = new System.Drawing.Size(23, 23);
+			this.deleteAnimationStepButton.TabIndex = 4;
+			this.deleteAnimationStepButton.Text = "-";
+			this.deleteAnimationStepButton.UseVisualStyleBackColor = true;
+			this.deleteAnimationStepButton.Click += new System.EventHandler(this.deleteAnimationStepButton_Click);
+			// 
+			// upAnimationStepButton
+			// 
+			this.upAnimationStepButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.upAnimationStepButton.BackgroundImage = global::BahnEditor.Editor.Properties.Resources.uparrow;
+			this.upAnimationStepButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.upAnimationStepButton.Location = new System.Drawing.Point(389, 70);
+			this.upAnimationStepButton.Name = "upAnimationStepButton";
+			this.upAnimationStepButton.Size = new System.Drawing.Size(23, 23);
+			this.upAnimationStepButton.TabIndex = 5;
+			this.upAnimationStepButton.UseVisualStyleBackColor = true;
+			this.upAnimationStepButton.Click += new System.EventHandler(this.upAnimationStepButton_Click);
+			// 
+			// downAnimationStepButton
+			// 
+			this.downAnimationStepButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.downAnimationStepButton.BackgroundImage = global::BahnEditor.Editor.Properties.Resources.downarrow;
+			this.downAnimationStepButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.downAnimationStepButton.Location = new System.Drawing.Point(389, 99);
+			this.downAnimationStepButton.Name = "downAnimationStepButton";
+			this.downAnimationStepButton.Size = new System.Drawing.Size(23, 23);
+			this.downAnimationStepButton.TabIndex = 6;
+			this.downAnimationStepButton.UseVisualStyleBackColor = true;
+			this.downAnimationStepButton.Click += new System.EventHandler(this.downAnimationStepButton_Click);
+			// 
 			// AnimationForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(485, 370);
+			this.ClientSize = new System.Drawing.Size(424, 332);
+			this.Controls.Add(this.downAnimationStepButton);
+			this.Controls.Add(this.upAnimationStepButton);
+			this.Controls.Add(this.deleteAnimationStepButton);
+			this.Controls.Add(this.addAnimationStepButton);
+			this.Controls.Add(this.noAnimationLabel);
 			this.Controls.Add(this.dataGridView);
 			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(440, 172);
 			this.Name = "AnimationForm";
 			this.Text = "Animations";
 			this.Load += new System.EventHandler(this.AnimationForm_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -102,5 +180,10 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn minTimeColumn;
 		private System.Windows.Forms.DataGridViewTextBoxColumn maxTimeColumn;
 		private System.Windows.Forms.DataGridViewTextBoxColumn soundColumn;
+		private System.Windows.Forms.Label noAnimationLabel;
+		private System.Windows.Forms.Button addAnimationStepButton;
+		private System.Windows.Forms.Button deleteAnimationStepButton;
+		private System.Windows.Forms.Button upAnimationStepButton;
+		private System.Windows.Forms.Button downAnimationStepButton;
 	}
 }
--- BahnEditor/BahnEditor.Editor/AnimationForm.cs
+++ BahnEditor/BahnEditor.Editor/AnimationForm.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BahnEditor.BahnLib;
 
 namespace BahnEditor.Editor
 {
 	public partial class AnimationForm : Form
 	{
 		private Editor editor;
+		private Rectangle dragBoxFromMouseDown;
+		private int rowIndexFromMouseDown;
+		private int rowIndexOfItemUnderMouseToDrop;
 
 		public AnimationForm()
 		{
@@ -33,16 +38,186 @@ namespace BahnEditor.Editor
 		internal bool ChangeAnimationProgram()
 		{
 			this.dataGridView.Rows.Clear();
-			if(this.editor.Zoom1Archive != null && this.editor.Zoom1Archive.Animation != null && this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID] != null)
+			if (this.editor.Zoom1Archive != null && this.editor.Zoom1Archive.Animation != null && this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID] != null)
 			{
-				BahnLib.AnimationProgram program = this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID];
+				this.dataGridView.Visible = true;
+				this.addAnimationStepButton.Visible = true;
+				this.deleteAnimationStepButton.Visible = true;
+				this.upAnimationStepButton.Visible = true;
+				this.downAnimationStepButton.Visible = true;
+				this.noAnimationLabel.Visible = false;
+				AnimationProgram program = this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID];
 				for (int i = 0; i < program.AnimationStepCount; i++)
 				{
-					this.dataGridView.Rows.Add(program[i].AnimationPhase, program[i].MinimumTime, program[i].MaximumTime, program[i].Sound);
+					this.dataGridView.Rows.Add(program[i].AnimationPhase.ToString(CultureInfo.InvariantCulture), program[i].MinimumTime.ToString(CultureInfo.InvariantCulture), program[i].MaximumTime.ToString(CultureInfo.InvariantCulture), program[i].Sound.ToString(CultureInfo.InvariantCulture));
 				}
 				return true;
 			}
+			this.dataGridView.Visible = false;
+			this.addAnimationStepButton.Visible = false;
+			this.deleteAnimationStepButton.Visible = false;
+			this.upAnimationStepButton.Visible = false;
+			this.downAnimationStepButton.Visible = false;
+			this.noAnimationLabel.Visible = true;
 			return false;
+		}
+
+		private void addAnimationStepButton_Click(object sender, EventArgs e)
+		{
+			this.dataGridView.Rows.Add("0", "0", "0", "0");
+			this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID].AddAnimationStep(new AnimationStep(0, 0, 0, 0));
+		}
+
+		private void deleteAnimationStepButton_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Should the entry be deleted?", "Delete Animationstep", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+			{
+				return;
+			}
+			else
+			{
+				this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID].RemoveAnimationStep(this.dataGridView.CurrentCell.RowIndex);
+				this.dataGridView.Rows.RemoveAt(this.dataGridView.CurrentCell.RowIndex);
+			}
+			
+		}
+
+		private void upAnimationStepButton_Click(object sender, EventArgs e)
+		{
+			this.MoveStep(this.dataGridView.CurrentCell.RowIndex, this.dataGridView.CurrentCell.RowIndex - 1);
+		}
+
+		private void downAnimationStepButton_Click(object sender, EventArgs e)
+		{
+			this.MoveStep(this.dataGridView.CurrentCell.RowIndex, this.dataGridView.CurrentCell.RowIndex + 1);
+		}
+
+		private void dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+		{
+			
+		}
+
+		private void dataGridView_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+		{
+			if (MessageBox.Show("Should the entry be deleted?", "Delete Animationstep", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+			{
+				e.Cancel = true;
+			}
+			else
+			{
+				this.editor.Zoom1Archive.Animation[this.editor.ActualGraphicID, this.editor.ActualAlternativeID].RemoveAnimationStep(e.Row.Index);
+			}
+		}
+
+		private void dataGridView_MouseMove(object sender, MouseEventArgs e)
+		{
+			if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+			{
+				// If the mouse moves outside the rectangle, start the drag.
+				if (dragBoxFromMouseDown != Rectangle.Empty &&
+					!dragBoxFromMouseDown.Contains(e.X, e.Y))
+				{
+
+					// Proceed with the drag and drop, passing in the list item.                    
+					DragDropEffects dropEffect = dataGridView.DoDragDrop(
+					dataGridView.Rows[rowIndexFromMouseDown],
+					DragDropEffects.Move);
+				}
+			}
+		}
+
+		private void dataGridView_MouseDown(object sender, MouseEventArgs e)
+		{
+			// Get the index of the item the mouse is below.
+			rowIndexFromMouseDown = dataGridView.HitTest(e.X, e.Y).RowIndex;
+			if (rowIndexFromMouseDown != -1)
+			{
+				// Remember the point where the mouse down occurred. 
+				// The DragSize indicates the size that the mouse can move 
+				// before a drag event should be started.                
+				Size dragSize = SystemInformation.DragSize;
+
+				// Create a rectangle using the DragSize, with the mouse position being
+				// at the center of the rectangle.
+				dragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+															   e.Y - (dragSize.Height / 2)),
+																dragSize);
+			}
+			else
+				// Reset the rectangle if the mouse is not over an item in the ListBox.
+				dragBoxFromMouseDown = Rectangle.Empty;
+		}
+
+		private void dataGridView_DragOver(object sender, DragEventArgs e)
+		{
+			e.Effect = DragDropEffects.Move;
+		}
+
+		private void dataGridView_DragDrop(object sender, DragEventArgs e)
+		{
+			// The mouse locations are relative to the screen, so they must be 
+			// converted to client coordinates.
+			Point clientPoint = dataGridView.PointToClient(new Point(e.X, e.Y));
+
+			// Get the row index of the item the mouse is below. 
+			rowIndexOfItemUnderMouseToDrop =
+				dataGridView.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+			// If the drag operation was a move then remove and insert the row.
+			if (e.Effect == DragDropEffects.Move)
+			{
+				//DataGridViewRow rowToMove = e.Data.GetData(
+				//	typeof(DataGridViewRow)) as DataGridViewRow;
+				//dataGridView.Rows.RemoveAt(rowIndexFromMouseDown);
+				//dataGridView.Rows.Insert(rowIndexOfItemUnderMouseToDrop, rowToMove);
+				this.MoveStep(rowIndexFromMouseDown, rowIndexOfItemUnderMouseToDrop);
+			}
+		}
+
+		private void MoveStep(int actualRow, int targetRow)
+		{
+			if (actualRow == -1 || targetRow == -1 || targetRow >= dataGridView.Rows.Count || targetRow >= editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID].AnimationStepCount)
+				return;
+			DataGridViewRow rowToMove = dataGridView.Rows[actualRow];
+			dataGridView.Rows.RemoveAt(actualRow);
+			dataGridView.Rows.Insert(targetRow, rowToMove);
+			AnimationStep step = this.editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID][actualRow];
+			this.editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID].RemoveAnimationStep(actualRow);
+			this.editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID].InsertAnimationStep(step, targetRow);
+			this.dataGridView.CurrentCell = this.dataGridView.Rows[targetRow].Cells[0];
+		}
+
+		private void dataGridView_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+		{
+			if (e.RowIndex >= 0)
+			{
+				int value = 0;
+				bool result = int.TryParse(e.FormattedValue as String, NumberStyles.Integer, CultureInfo.CurrentCulture, out value);
+				if (!result)
+				{
+					MessageBox.Show("Value is not a number", "Invalid Value", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					e.Cancel = true;
+					return;
+				}
+				switch (e.ColumnIndex)
+				{
+					case 0:
+						editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID][e.RowIndex].AnimationPhase = value;
+						break;
+					case 1:
+						editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID][e.RowIndex].MinimumTime = value;
+						break;
+					case 2:
+						editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID][e.RowIndex].MaximumTime = value;
+						break;
+					case 3:
+						editor.Zoom1Archive.Animation[editor.ActualGraphicID, editor.ActualAlternativeID][e.RowIndex].Sound = value;
+						break;
+					default:
+						MessageBox.Show("Unknown Column", "Unknown Column", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						break;
+				}
+			}
 		}
 	}
 }
